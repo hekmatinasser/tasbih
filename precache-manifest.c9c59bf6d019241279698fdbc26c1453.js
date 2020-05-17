7192f8dc80722ee509724ff578eef76f0df0346c
Nasser <hekmati.nasser@gmail.com>
2020-05-17 21:18:52 +0430

    init commit

---
+++ precache-manifest.c9c59bf6d019241279698fdbc26c1453.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0e127b63e1d9ce6d4c",
+    "url": "/tasbih/css/app.0a9a11aa.css"
+  },
+  {
+    "revision": "c2c838bbf0fbb5cb2c3e",
+    "url": "/tasbih/css/chunk-vendors.4b4b128c.css"
+  },
+  {
+    "revision": "15fbbcc65458d7c4304218e410383b8a",
+    "url": "/tasbih/data/posts.json"
+  },
+  {
+    "revision": "66c197702df5c71b8a938084cd98a3aa",
+    "url": "/tasbih/data/تسبیحات_فاطمه_زهرا.json"
+  },
+  {
+    "revision": "12a0eed7e8c6264efdd3eb296e58a955",
+    "url": "/tasbih/data/نماز_شب.json"
+  },
+  {
+    "revision": "05727d32400b2008acbf7fc49251ede0",
+    "url": "/tasbih/fonts/Yekan.05727d32.woff"
+  },
+  {
+    "revision": "1008c3b88ceb6f09efbf88dafa3e9d91",
+    "url": "/tasbih/fonts/Yekan.1008c3b8.ttf"
+  },
+  {
+    "revision": "1ce443bdb97d708d21442761cc7c441d",
+    "url": "/tasbih/fonts/Yekan.1ce443bd.eot"
+  },
+  {
+    "revision": "1ce443bdb97d708d21442761cc7c441d",
+    "url": "/tasbih/fonts/Yekan.eot"
+  },
+  {
+    "revision": "a770561bd4a828ceb14c870ed3f26ba5",
+    "url": "/tasbih/fonts/Yekan.otf"
+  },
+  {
+    "revision": "77c6c4d79045178bda720f56e2e026b7",
+    "url": "/tasbih/fonts/Yekan.svg"
+  },
+  {
+    "revision": "1008c3b88ceb6f09efbf88dafa3e9d91",
+    "url": "/tasbih/fonts/Yekan.ttf"
+  },
+  {
+    "revision": "05727d32400b2008acbf7fc49251ede0",
+    "url": "/tasbih/fonts/Yekan.woff"
+  },
+  {
+    "revision": "77c6c4d79045178bda720f56e2e026b7",
+    "url": "/tasbih/img/Yekan.77c6c4d7.svg"
+  },
+  {
+    "revision": "0de746d15ce3ab5928b5e9e523d03f3e",
+    "url": "/tasbih/img/logo.0de746d1.png"
+  },
+  {
+    "revision": "6fe4728119c3100c290325f597a5fe0c",
+    "url": "/tasbih/index.html"
+  },
+  {
+    "revision": "5c0e127b63e1d9ce6d4c",
+    "url": "/tasbih/js/app.bdd6172d.js"
+  },
+  {
+    "revision": "c2c838bbf0fbb5cb2c3e",
+    "url": "/tasbih/js/chunk-vendors.8d2120dd.js"
+  },
+  {
+    "revision": "833dd45c628cf4b381ec328db528abf0",
+    "url": "/tasbih/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tasbih/robots.txt"
+  }
+]);
